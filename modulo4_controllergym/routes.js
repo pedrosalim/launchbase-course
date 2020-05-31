adce5a9ce2dfb268ffedb440f9a482382039f65a
Pedro <pedrof.salim@gmail.com>
2020-05-31 01:01:31 -0300

    controller gym

--- modulo4_controllergym/routes.js
+++ modulo4_controllergym/routes.js
@@ -9,6 +9,23 @@ routes.get('/instructors', function(req, res) {
     return res.render("instructors/index")
 })
 
+routes.get('/instructors/create', function(req, res) {
+    return res.render("instructors/create")
+})
+
+routes.post('/instructors', function(req, res) {
+
+    const keys = Object.keys(req.body)
+    // VERIFICAR SE ALGUMA CHAVE ESTÁ VAZIA, SE ESTIVER RETORNA MENSAGEM
+    for (key of keys) {
+        if (req.body[key] == "") {
+            return res.send("Please, fill all fields")
+        }
+    }
+
+    return res.send(keys)
+})
+
 routes.get('/members', function(req, res) {
     return res.send("members")
 })
