14f73902cb92d09a76661b094fa6e2329856eb12
Pedro <pedrof.salim@gmail.com>
2020-06-13 00:58:44 -0300

    routes

--- aulasparticulares/routes.js
+++ aulasparticulares/routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = express.Router()
+const teachers = require('./teachers')
 
 routes.get('/', function(req, res) {
     return res.redirect("/instructors")
@@ -13,18 +14,7 @@ routes.get('/instructors/create', function(req, res) {
     return res.render('instructors/create')
 })
 
-routes.post('/instructors', function(req, res) {
-
-    const keys = Object.keys(req.body)
-
-    for(key of keys) {
-        if (req.body[key] == "") {
-            return res.send('Please, fill all fields')
-        }
-    }
-
-    return res.send(req.body)
-})
+routes.post('/instructors', teachers.post)
 
 routes.get('/students', function(req, res) {
     return res.send('students')
